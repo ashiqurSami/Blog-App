906d585d22ec4c76e8f7dddbbdc882b90edf56bb
ashiqursami <2020741@iub.edu.bd>
2024-04-29 02:19:44 +0600

    initial commit and basic pages created

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Project from "./pages/Project";
+
+const App = () => {
+  return (
+    <BrowserRouter >
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/projects' element={<Project />} />
+      </Routes>
+    </BrowserRouter >
+  );
+};
+
+export default App;
