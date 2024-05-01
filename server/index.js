f57fdfc02b34eb7b3d00064a4d15c6038ede8eb9
ashiqursami <2020741@iub.edu.bd>
2024-05-01 07:50:01 +0600

    user model created

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
 
-
+mongoose
+  .connect(process.env.MONGO_STRING)
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err).toString();
+  });
 
 const app = express();
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
-  });
+  console.log("Server is running on port 3000!");
+});
