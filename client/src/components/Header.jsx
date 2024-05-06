313be39108a9b3a96916ee7ca2cfca18b5fdea91
ashiqursami <2020741@iub.edu.bd>
2024-05-06 06:42:58 +0600

    added functionality in sign up component but still button issue persist

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,59 +1,65 @@
-import React from "react";
-import { Navbar, TextInput,Button } from "flowbite-react";
-import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
-import { Link, useLocation } from "react-router-dom";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaSun } from 'react-icons/fa';
 
-
-const Header = () => {
-    const path = useLocation().pathname;
+export default function Header() {
 
   return (
-    <Navbar className="border-b-2">
+    <Navbar className='border-b-2'>
       <Link
-        to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
           Sami's
         </span>
         Blog
       </Link>
-      <TextInput
-        type="text"
-        placeholder="search..."
-        rightIcon={AiOutlineSearch}
-        className="hidden lg:inline"
-      ></TextInput>
-      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+      
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+        />
+      
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch />
       </Button>
-      <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon></FaMoon>
+      <div className='flex gap-2 md:order-2'>
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+          <FaSun />
         </Button>
-        <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>
+        <Dropdown arrowIcon={false} inline label={<Avatar alt='user' img='currentUser.profilePicture' rounded />}>
+          <Dropdown.Header>
+            <span className='block text-sm'>username</span>
+            <span className='block text-sm font-medium truncate'>email</span>
+          </Dropdown.Header>
+          <Link to="">
+            <Dropdown.Item>Profile</Dropdown.Item>
+          </Link>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
+        <Link to='/sign-in'>
+          <Button gradientDuoTone='purpleToBlue' outline>
             Sign In
           </Button>
         </Link>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={path === "/"} as={"div"}>
-          <Link to="/">Home</Link>
+        <Navbar.Link active={false} as={'div'}>
+          <Link to='/'>Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/about"} as={"div"}>
-          <Link to="/about">About</Link>
+        <Navbar.Link active={false} as={'div'}>
+          <Link to='/about'>About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/projects"} as={"div"}>
-          <Link to="/projects">Projects</Link>
+        <Navbar.Link active={false} as={'div'}>
+          <Link to='/projects'>Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
-};
-
-// Rest of the code...
-
-export default Header;
+}
