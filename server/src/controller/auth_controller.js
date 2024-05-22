b668a3428648015ec87abc89d50bc1676ad8c298
ashiqursami <2020741@iub.edu.bd>
2024-05-23 03:14:29 +0600

    signin api not completed

--- server/src/controller/auth_controller.js
+++ server/src/controller/auth_controller.js
@@ -1,8 +1,8 @@
 import User from "../model/user.js";
 import { hashPassword } from "./../helper/auth_helper.js";
-import {error_handler} from "./../utils/error.js";
+import { error_handler } from "./../utils/error.js";
 
-export const signup = async (req, res,next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (
     !username ||
@@ -12,7 +12,7 @@ export const signup = async (req, res,next) => {
     email == "" ||
     password == ""
   ) {
-    return next(error_handler(400, "All fields are required"))
+    return next(error_handler(400, "All fields are required"));
   }
 
   const hashedPassword = hashPassword(password);
@@ -26,6 +26,35 @@ export const signup = async (req, res,next) => {
     await newUser.save();
     res.json({ message: "sign up successfully" });
   } catch (error) {
-    next(error)
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email == "" || password == "") {
+    return next(error_handler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(error_handler(400, "User not found"));
+    }
+    const validPassword = await bcryptjs.compareSync(
+      password,
+      validUser.password
+    );
+    if (!validPassword) {
+      return next(error_handler(400, "Wrong password"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cokkie("access_token", token, { httpOnly: true })
+      .json(rest);
+  } catch (e) {
+    next(e);
   }
 };
